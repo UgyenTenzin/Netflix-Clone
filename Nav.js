30510a4e88d3d746c025c549c8fa7fe92fa46d15
Ugyen Tenzin <71688792+UgyenTenzin@users.noreply.github.com>
2020-09-22 18:50:09 +0800

    Add files via upload

---
+++ Nav.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from "react";
+import "./Nav.css";
+
+function Nav() {
+  const [show, handleShow] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+  return (
+    <div className={`nav ${show && "nav__black"}`}>
+      <img
+        className="nav__logo"
+        src="https://logos-world.net/wp-content/uploads/2020/04/Netflix-Logo-700x394.png"
+        alt="netflix logo"
+      />
+      <img
+        className="nav__avatar"
+        src="https://pro2-bar-s3-cdn-cf1.myportfolio.com/dddb0c1b4ab622854dd81280840458d3/98032aebff601c1d993e12a0_rw_600.png?h=8030f4d5734548795c22da59ca72e3e1"
+        alt="Netflix logo"
+      />
+    </div>
+  );
+}
+
+export default Nav;
